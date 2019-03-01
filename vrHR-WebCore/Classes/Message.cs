44ac79ed34f1afd7243204ea2221240bb8ba55ab
CruciformHawk7 <nikhil_hari@live.com>
2019-03-02 04:09:53 +0530

    Added class partially added a few css added signup fixed links

---
+++ vrHR-WebCore/Classes/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vrHR_WebCore.Classes
+{
+    public class Message
+    {
+        public int MessageID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
--- vrHR-WebCore/Pages/SignUp.cshtml.cs
+++ vrHR-WebCore/Pages/SignUp.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using vrHR_WebCore.Classes;
 
 namespace vrHR_WebCore.Pages
 {
     public class SignUpModel : PageModel
     {
-        public void OnGet()
-        {
+        [BindProperty]
+        public HR hr { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        [BindProperty]
+        public string ConfirmPassword { get; set; }
 
+        public string abc { get; set; }
+        public void OnPost()
+        {
+            abc = hr.FirstName + " " + hr.LastName;
         }
     }
 }
